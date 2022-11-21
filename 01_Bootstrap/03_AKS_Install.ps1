e6a5aa41540bff7f768fe494326b83a755ba9a95
ahelland <ahelland@users.noreply.github.com>
2022-11-21 09:18:12 +0100

    October 2022 version bump

--- 01_Bootstrap/03_AKS_Install.ps1
+++ 01_Bootstrap/03_AKS_Install.ps1
@@ -1,6 +1,6 @@
 # Adapt variables to your liking. Defaults provided.
 # Network settings are just placeholders and will most likely need to be adjusted.
-$k8sVersion             = "v1.23.8"
+$k8sVersion             = "v1.24.6"
 $clusterName            = "aks01"
 $vnetName               = "aksvnet"
 $vnetSwitch             = "LAN"
